5fa600d4382ca025ec23fef4bfe819b7e63fc597
Tychcl <tocabloha@gmail.com>
2025-05-13 05:36:07 +0500

    AI50%ready

--- handlers/AI.py
+++ handlers/AI.py
@@ -1,6 +1,6 @@
 from create_bot import bot
 from aiogram import Router, F
-from aiogram.types import Message,CallbackQuery, FSInputFile, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import Message,CallbackQuery, FSInputFile, InlineKeyboardMarkup, InlineKeyboardButton, InputMediaPhoto
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from keyboards.AI_inln import inkb, format, keyboard, prompt
@@ -17,8 +17,9 @@ from g4f import Client
 
 AI_router = Router()
 
-def Use_AI(func: function, *args):
+async def Use_AI(func, *args):
     result_queue = Queue()
+    args = args + (result_queue,)
     process = Process(target=func, args=args)
     process.start()
     process.join()
@@ -40,9 +41,8 @@ class Image(StatesGroup):
     StartMessageId = State()
     LastInline = State()
 
-def Get_Image(Prompt: str, Width: int, Height: int, result_queue):
-    client = Client()
-    response = client.images.generate(
+def Get_Image(Client: Client, Prompt: str, Width: int, Height: int, result_queue):
+    response = Client.images.generate(
     model="dall-e-3",
     prompt=Prompt,
     width=Width,
@@ -60,7 +60,9 @@ async def Start_Image(call: CallbackQuery, state: FSMContext):
     reply, text = format()
     await msg.edit_text(text=text, reply_markup=reply)
 
-@AI_router.callback_query(F.data.contains('square.') or F.data.contains('land.') or F.data.contains('port.'))
+@AI_router.callback_query(F.data.contains('square.'))
+@AI_router.callback_query(F.data.contains('land.'))
+@AI_router.callback_query(F.data.contains('port.'))
 async def Sqare_Format(call: CallbackQuery, state: FSMContext):
     wxh = call.data.split('.')
     await state.update_data(Width = int(wxh[1]))
@@ -74,22 +76,46 @@ async def Sqare_Format(call: CallbackQuery, state: FSMContext):
 async def AI_Image_Generation(message: Message, state: FSMContext):
     await state.update_data(Prompt = message.text)
     data = await state.get_data()
-    result_queue = Queue()
-    process = Process(target=Get_Image, args=(data['Prompt'], data['Width'], data['Height'], result_queue))
-    process.start()
-    process.join()
-    url = result_queue.get()
+    translate = [{
+        "role": "user",
+        "content": f"Translate the following text to English: {data['Prompt']}, only answer without other text pls"
+    }]
+    client = Client()
+    try:
+        Prompt = await Use_AI(Get_Answer, client, translate)
+        url = await Use_AI(Get_Image, client, Prompt.content, data['Width'], data['Height'])
+    except:
+        await state.clear()
+        await state.set_state(None)
+        await message.answer("Ошибка😒")
+        return
+    media = [InputMediaPhoto(media=url, caption=message.text)]
+    await message.answer_media_group(media=media)
+    await message.delete()
+    await state.clear()
+    await state.set_state(None)
+
+
 #ГЕНЕРАЦИЯ ОТВЕТОВ
 
 class AIText(StatesGroup):
     context = State()
     prompt = State()
 
+def Get_Answer(Client:Client, Context, result_queue):
+    response = Client.chat.completions.create(
+        model="deepseek-ai/DeepSeek-V3",
+        messages=Context 
+    )
+    result_queue.put(response.choices[0].message)
+
 @AI_router.callback_query(F.data == 'back')  # Сработает при команде /cancel
 async def back_handler(call: CallbackQuery, state: FSMContext) -> None:
     data = await state.get_data()
     reply, text = data['LastInline']
     await call.message.edit_text(text=text, reply_markup=reply)
+    await state.clear()
+    await state.set_state(None)
 
 #ОТМЕНА
 @AI_router.callback_query(F.data.contains('cancel'))  # Сработает при команде /cancel
